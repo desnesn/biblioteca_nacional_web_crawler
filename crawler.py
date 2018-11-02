bed22179720b0ba9c98f9a75bd72ef58f2a95959
Desnes A. Nunes do Rosario <desnesn@gmail.com>
2018-11-02 14:48:17 -0300

    Web scrapping da biblioteca nacional digital

---
+++ crawler.py
@@ -0,0 +1,46 @@
+# -*- coding: latin-1 -*-
+import csv
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.action_chains import ActionChains
+
+bib="386030"
+pesquisa="populista"
+url = "http://memoria.bn.br/docreader/DocReader.aspx?bib=" + bib + "&Pesq=" + pesquisa
+
+out = open("memoria.bn.br_" + bib + "_" + pesquisa + ".csv", 'w')
+out.write("Ano,Edição,link\n")
+
+chrome_options = Options()  
+chrome_options.add_argument("--headless")  
+driver = webdriver.Chrome(chrome_options=chrome_options)
+
+driver.get(url)
+
+cur, tot = driver.find_element_by_id("OcorNroLbl").text.strip().split("/")
+
+for i in range(1,int(tot)):
+    pag = driver.find_element_by_id("hPagFis").get_attribute("value")
+    ano, edi = driver.find_element_by_id("PastaTxt").get_attribute("title").strip().split("\\")
+    link = "http://memoria.bn.br/docreader/" + bib + "/" + pag
+
+    print edi
+    print "[" + str(i) + "]" + link
+    print
+
+    out.write(ano.encode('utf-8')[4:] + "," + edi.encode('utf-8')[9:] + "," + link.encode('utf-8') + "\n")
+
+    next_pg = driver.find_element_by_id("OcorPosBtn")
+    ActionChains(driver).move_to_element(next_pg).click(next_pg).perform()
+
+    WebDriverWait(driver, 15).until(
+            expected_conditions.text_to_be_present_in_element(
+                (By.ID, 'OcorNroLbl'), str(i+1)+'/'+tot)
+            )
+
+out.close()
+driver.quit()
