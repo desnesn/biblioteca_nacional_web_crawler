788a424a1190db7cc6dcda069dd29781e587f8e2
Desnes A. Nunes do Rosario <desnesn@gmail.com>
2018-11-03 10:41:24 -0300

    Fix last input entrance

--- crawler.py
+++ crawler.py
@@ -23,24 +23,23 @@ driver.get(url)
 
 cur, tot = driver.find_element_by_id("OcorNroLbl").text.strip().split("/")
 
-for i in range(1,int(tot)):
+for i in range(1,int(tot)+1):
     pag = driver.find_element_by_id("hPagFis").get_attribute("value")
     ano, edi = driver.find_element_by_id("PastaTxt").get_attribute("title").strip().split("\\")
     link = "http://memoria.bn.br/docreader/" + bib + "/" + pag
 
-    print edi
-    print "[" + str(i) + "]" + link
-    print
-
     out.write(ano.encode('utf-8')[4:] + "," + edi.encode('utf-8')[9:] + "," + link.encode('utf-8') + "\n")
 
-    next_pg = driver.find_element_by_id("OcorPosBtn")
-    ActionChains(driver).move_to_element(next_pg).click(next_pg).perform()
+    print "[" + str(i) + "/" + tot + "] - " + ano.encode('utf-8')[4:] + "\\" + edi.encode('utf-8')[9:] + " - " + link.encode('utf-8')
+    print
 
-    WebDriverWait(driver, 15).until(
+    if (i!=int(tot)):
+        next_pg = driver.find_element_by_id("OcorPosBtn")
+        ActionChains(driver).move_to_element(next_pg).click(next_pg).perform()
+        WebDriverWait(driver, 30).until(
             expected_conditions.text_to_be_present_in_element(
                 (By.ID, 'OcorNroLbl'), str(i+1)+'/'+tot)
-            )
+        )
 
 out.close()
 driver.quit()
